f21b84e43328b6b87ba19bd3f87066651910ce82
misternavet <132678563+misternavet@users.noreply.github.com>
2023-05-05 16:27:14 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+// basic.show_string(convert_to_text(input.light_level()))
+basic.pause(2000)
+basic.forever(function () {
+    basic.showNumber(input.temperature())
+    // basic.show_string(convert_to_text(input.light_level()))
+    basic.pause(1000)
+})
